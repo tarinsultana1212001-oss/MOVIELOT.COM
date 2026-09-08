29bde0f34fac97183f5a2c1ef454148e4d661e13
tarinsultana1212001-oss <tarinsultana1212001@gmail.com>
2026-09-09 01:07:59 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
-
 export default defineConfig(() => {
   return {
+    base: '/MOVIELOT.COM/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
