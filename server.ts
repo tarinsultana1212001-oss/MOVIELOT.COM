b217b66197708aeea90febc0213be3072b0837c4
tarinsultana1212001-oss <tarinsultana1212001@gmail.com>
2026-09-10 01:11:04 +0600

    Update server.ts

--- server.ts
+++ server.ts
@@ -7,7 +7,7 @@ import { AuthService } from './server/services/authService';
 import { User } from './src/types';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json({ limit: '1mb' }));
 
